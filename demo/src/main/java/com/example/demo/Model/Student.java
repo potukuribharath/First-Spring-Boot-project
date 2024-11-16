89674f4ba5daaa0504b7b475e17b0ddd03fd4f1a
potukuribharath <103629063+potukuribharath@users.noreply.github.com>
2024-11-17 00:42:55 +0530

    new changes

--- demo/src/main/java/com/example/demo/Model/Student.java
+++ demo/src/main/java/com/example/demo/Model/Student.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//entity
 @Entity
 @Table(name="stud")
 public class Student {
